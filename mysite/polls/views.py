b2a0538c51e0f4398901f6e2ace04e17d3f447e1
jSeastar <Jseastar92@gmail.com>
2020-07-28 21:04:04 +0900

    장고 튜토리얼 Part3 시작! - 뷰 추가하기 까지 ( 오늘은 너무 피곤........ 눈이 침침하다 )

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(reqeust):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(reqeust, question_id):
+    HttpResponse = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(reqeust, question_id):
+    return HttpResponse("You're voitng on quesiton %s." % question_id)
