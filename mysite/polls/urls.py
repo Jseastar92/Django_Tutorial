7f0027b701b6a3b13583ca9678d5443e8a1c99bd
jSeastar <Jseastar92@gmail.com>
2020-08-04 23:46:17 +0900

    Part4 write minimal form

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     
     # ex : /polls/results
-    # path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     
     # ex : /polls/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
